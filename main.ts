0ef94d7eb216456ab22fe0cc64fd0ea1d8707b2c
Eric <eric.lorenzo@gracia.lasalle.cat>
2025-12-04 18:44:35 +0100

    Extra

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber > dice) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+        if (receivedNumber < dice) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Asleep)
+        }
+    }
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendString("Hola")
     basic.showString("ENVIAT!")
@@ -5,7 +16,10 @@ input.onButtonPressed(Button.A, function () {
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
-radio.setGroup(1)
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    dice = randint(1, 6)
+    basic.showNumber(dice)
+    radio.sendNumber(dice)
 })
+let dice = 0
+radio.setGroup(1)
